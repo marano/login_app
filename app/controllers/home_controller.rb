fde7450f827e62499db5649a554dfb197cf1d719
marano <thiagomarano@gmail.com>
2016-12-19 21:45:06 -0200

    Redirect from home to login unless user is logged in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  before_action :require_session
+
   def index
+  end
+
+  private
+
+  def require_session
+    redirect_to new_session_path unless session[:logged_in]
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,7 +4,15 @@ RSpec.describe HomeController do
   describe '#index' do
     subject { get :index }
 
-    it { is_expected.to be_success }
-    it { is_expected.to render_template(:index) }
+    context 'logged in' do
+      before { session[:logged_in] = true }
+
+      it { is_expected.to be_success }
+      it { is_expected.to render_template(:index) }
+    end
+
+    context 'logged out' do
+      it { is_expected.to redirect_to(new_session_path) }
+    end
   end
 end
