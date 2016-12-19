65b54ff9068b7ad4c5f224078c65dcb198d4efaa
marano <thiagomarano@gmail.com>
2016-12-19 21:53:16 -0200

    Sets new session on new action.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe SessionsController do
   describe '#new' do
-    subject { get :new }
+    subject! { get :new }
 
     it { is_expected.to be_success }
     it { is_expected.to render_template(:new) }
+    it { expect(assigns(:session)).to be_present }
   end
 
   describe '#create' do
