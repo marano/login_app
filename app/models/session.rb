27db8815b3fb27b5c26dd057c51873de36ef75ba
marano <thiagomarano@gmail.com>
2016-12-19 21:59:27 -0200

    Make session a tableless active record model.

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,16 +1,11 @@
 class Session
-  include ActiveModel::Validations
+  include ActiveModel::Model
 
   attr_accessor :email, :password
 
   validates :email, :password, length: { minimum: 8 }
 
   validate :validate_credentials
-
-  def initialize(attributes = {})
-    @email = attributes[:email]
-    @password = attributes[:password]
-  end
 
   private
 
